84a9f33091eace243c093066dce18bc7dda63c76
Pedro Semeano <semeano@gmail.com>
2016-12-05 13:50:41 +0000

    Package updates.

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
+	name: 'semeano:meteor-fb-catalog',
   summary: "Serves a catalog (csv) for Facebook dynamic ads",
   version: "0.0.1",
-  git: "https://github.com/semeano/meteor-fb-catalog.git"
+  git: "https://github.com/semeano/meteor-fb-catalog.git",
+  documentation: "README.md"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use('webapp@1.0.0', 'server');
 	api.add_files('catalog.js', 'server');
 	api.export('catalog', 'server');
